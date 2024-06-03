d5d94e81c66f4bdf5832338d2dbe4ae79fcda2ef
figlerr <figler.renata@tmx.hu>
2024-06-03 17:29:07 +0200

    forras

---
+++ src/test/java/hu/example/mavenexample/MavenExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.example.mavenexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
